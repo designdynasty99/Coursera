b71efbd9828602ec169bfa82d244e39e1d5112f0
Rahil <63215024+designdynasty99@users.noreply.github.com>
2020-04-27 19:35:34 +0530

    Javascript

---
+++ Module4/script.js
@@ -0,0 +1,39 @@
+
+  (function() {
+
+    var Yaakov = ["Yaakov"];
+    console.log ("Hello" + " " + [Yaakov])
+
+    var John = ["John"]
+    console.log ("Good Bye" + " " + [John])
+
+    var Jen = ["Jen"]
+    console.log ("Good Bye" + " " + [Jen])
+
+
+    var Jason = ["Jason"]
+    console.log ("Good Bye" + " " + [Jason])
+
+
+
+    var Paul = ["Paul"]
+    console.log ("Hello" + " " + [Paul])
+
+
+    var Frank =["Frank"]
+    console.log ("Hello" + " " + [Frank])
+
+
+    var Paula = ["Paula"]
+    console.log ("Hello" + " " + [Paula])
+
+
+    var Laura = ["Laura"]
+    console.log ("Hello" + " " + [Laura])
+
+
+    var Jim =["Jim"]
+    console.log ("Good Bye" + " " + [Jim])
+
+})();
+
